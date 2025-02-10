772abefe4df183f51defd5c8cfd2abfe618c792e
PedroHernandez173 <Phernande0152@gmail.com>
2025-02-10 15:18:51 -0800

    Initial Commit

---
+++ YouAreAwesomeNew/YouAreAwesomeNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeNewApp.swift
+//  YouAreAwesomeNew
+//
+//  Created by Student2 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
