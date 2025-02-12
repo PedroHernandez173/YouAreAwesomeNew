feab66eecd6c49db626f2596254e5c31d1136500
PedroHernandez173 <Phernande0152@gmail.com>
2025-02-12 14:27:10 -0800

    commit after the challenge was completed

--- YouAreAwesomeNew/ContentView.swift
+++ YouAreAwesomeNew/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            
+            Button("Press Me!") {
+                let message1 = "Sei Fantastico!"
+                let message2 = "Sei Grande!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+//                if message == message1{
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                
+                message = (message == message1 ? message2 : message1 )
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
+        
     }
 }
 
